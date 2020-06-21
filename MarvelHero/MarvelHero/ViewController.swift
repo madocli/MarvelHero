ae1e0d4ee9407f8b8c46335483423b04c1991123
Maria Donet <maria.donet@gmail.com>
2020-06-21 19:48:49 +0200

    initial project

---
+++ MarvelHero/MarvelHero/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MarvelHero
+//
+//  Created by Maria Donet Climent on 21/06/2020.
+//  Copyright © 2020 MariaDonet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
