2889189b16ede7f3f7c19a451a1b5a679826d64d
Maria Donet <maria.donet@gmail.com>
2020-06-22 20:38:53 +0200

    [ResponseModel] - Added CharacterResponseModel

---
+++ MarvelHero/MarvelHero/DataSource/RemoteDataSource/Model/CharacterResponseModel.swift
@@ -0,0 +1,105 @@
+//
+//  CharacterResponseModel.swift
+//  MarvelHero
+//
+//  Created by Maria Donet Climent on 22/06/2020.
+//  Copyright © 2020 MariaDonet. All rights reserved.
+//
+//   let codableProduct = try? newJSONDecoder().decode(CodableProduct.self, from: jsonData)
+
+import Foundation
+
+struct CharacterResponseModel: Codable {
+    let code: Int
+    let status, copyright, attributionText, attributionHTML: String?
+    let etag: String
+    let data: DataClass
+}
+
+// MARK: - DataClass
+struct DataClass: Codable {
+    let offset, limit, total, count: Int
+    let results: [Result]
+}
+
+// MARK: - Result
+struct Result: Codable {
+    let id: Int
+    let name, resultDescription: String
+    let modified: Date
+    let thumbnail: Thumbnail
+    let resourceURI: String
+    let comics, series: Comics
+    let stories: Stories
+    let events: Comics
+    let urls: [URLElement]
+
+    enum CodingKeys: String, CodingKey {
+        case id, name
+        case resultDescription = "description"
+        case modified, thumbnail, resourceURI, comics, series, stories, events, urls
+    }
+}
+
+// MARK: - Comics
+struct Comics: Codable {
+    let available: Int
+    let collectionURI: String
+    let items: [ComicsItem]
+    let returned: Int
+}
+
+// MARK: - ComicsItem
+struct ComicsItem: Codable {
+    let resourceURI: String
+    let name: String
+}
+
+// MARK: - Stories
+struct Stories: Codable {
+    let available: Int
+    let collectionURI: String
+    let items: [StoriesItem]
+    let returned: Int
+}
+
+// MARK: - StoriesItem
+struct StoriesItem: Codable {
+    let resourceURI: String
+    let name: String
+    let type: ItemType
+}
+
+enum ItemType: String, Codable {
+    case cover = "cover"
+    case empty = ""
+    case interiorStory = "interiorStory"
+}
+
+// MARK: - Thumbnail
+struct Thumbnail: Codable {
+    let path: String
+    let thumbnailExtension: Extension
+
+    enum CodingKeys: String, CodingKey {
+        case path
+        case thumbnailExtension = "extension"
+    }
+}
+
+enum Extension: String, Codable {
+    case gif = "gif"
+    case jpg = "jpg"
+}
+
+// MARK: - URLElement
+struct URLElement: Codable {
+    let type: URLType
+    let url: String
+}
+
+enum URLType: String, Codable {
+    case comiclink = "comiclink"
+    case detail = "detail"
+    case wiki = "wiki"
+}
